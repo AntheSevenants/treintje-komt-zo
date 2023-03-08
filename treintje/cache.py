74b97b1b720f25807ec2b984dd928cbdc33ad27e
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-03-08 20:58:24 +0100

    Add ability to diff caches

--- treintje/cache.py
+++ treintje/cache.py
@@ -1,8 +1,11 @@
 import json
 import time
+import json
+
 
 def create_cache_filename(from_station, to_station):
     return f"cache_{from_station}_{to_station}.json"
+
 
 def save_cache(from_station, to_station, events):
     cache = {"time": str(time.time()), "events": events}
@@ -10,3 +13,23 @@ def save_cache(from_station, to_station, events):
     cache_filename = create_cache_filename(from_station, to_station)
     with open(cache_filename, "wt") as writer:
         writer.write(json.dumps(cache))
+
+
+def diff_cache(from_station, to_station, new_events):
+    cache_filename = create_cache_filename(from_station, to_station)
+    with open(cache_filename, "rt") as reader:
+        old_cache = json.loads(reader.read())
+
+    # This list holds all new events which are different from the old events
+    truly_new_events = []
+
+    old_event_ids = list(map(lambda event: event["id"], old_cache["events"]))
+
+    for new_event in new_events:
+        if new_event["id"] in old_event_ids:
+            # This is not new information
+            continue
+
+        truly_new_events.append(new_event)
+
+    return truly_new_events
