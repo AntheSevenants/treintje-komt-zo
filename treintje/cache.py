373c832bd60001f85c7604c942d298cd6d3606c4
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-03-08 21:24:13 +0100

    Save cache epoch as int

--- treintje/cache.py
+++ treintje/cache.py
@@ -9,7 +9,7 @@ def create_cache_filename(from_station, to_station):
 
 
 def save_cache(from_station, to_station, events):
-    cache = {"time": str(time.time()), "events": events}
+    cache = {"time": int(time.time()), "events": events}
 
     cache_filename = create_cache_filename(from_station, to_station)
     with open(cache_filename, "wt") as writer:
