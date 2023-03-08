abf4a747ede0d31526adf79fe297cb95de219aa6
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-03-08 19:50:11 +0100

    Add scoped departure checks

--- treintje/events.py
+++ treintje/events.py
@@ -1,17 +1,28 @@
+from datetime import datetime
+import pytz
+
 import irail.api
 
+timezone = pytz.timezone('Europe/Brussels')
 
-
-def check(from_station, to_station):
+def check(from_station, to_station, checked_departure_times=[]):
     connections = irail.api.make_request(
         "connections", {"from": from_station, "to": to_station})
-    
+
     if not "connection" in connections:
         raise KeyError("Required 'connection' key not found in iRail response")
-    
+
     connections = connections["connection"]
 
     for connection in connections:
+        departure_epoch = int(connection["departure"]["time"])
+        timestamp = datetime.fromtimestamp(departure_epoch, timezone)
+        departure_time = timestamp.strftime('%H:%M')
+
+        if departure_time not in checked_departure_times:
+            print("Skipping the", departure_time, "train; not in checked departures")
+            continue
+
         if int(connection["departure"]["delay"]) > 0:
             print("Delay")
             # TODO: an event
