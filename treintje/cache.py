3217cbe99d326fc2089d1778d602a57af0322eb0
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-03-08 20:34:16 +0100

    Generalise filename creation

--- treintje/cache.py
+++ treintje/cache.py
@@ -1,9 +1,12 @@
 import json
 import time
 
+def create_cache_filename(from_station, to_station):
+    return f"cache_{from_station}_{to_station}.json"
 
 def save_cache(from_station, to_station, events):
     cache = {"time": str(time.time()), "events": events}
 
-    with open(f"cache_{from_station}_{to_station}.json", "wt") as writer:
+    cache_filename = create_cache_filename(from_station, to_station)
+    with open(cache_filename, "wt") as writer:
         writer.write(json.dumps(cache))
