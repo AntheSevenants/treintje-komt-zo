7da18ba8b14b526b639107aa3f4e00fb075d0c73
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-03-08 20:01:09 +0100

    Include departure time ion event

--- treintje/events.py
+++ treintje/events.py
@@ -6,8 +6,8 @@ import irail.api
 timezone = pytz.timezone('Europe/Brussels')
 
 
-def create_event(event_type, event_value=None):
-    return {"type": event_type, "value": event_value}
+def create_event(departure_time, event_type, event_value=None):
+    return {"departure_time": departure_time, "type": event_type, "value": event_value}
 
 
 def check(from_station, to_station, checked_departure_times=[]):
@@ -29,14 +29,14 @@ def check(from_station, to_station, checked_departure_times=[]):
         if departure_time not in checked_departure_times:
             print("Skipping the", departure_time,
                   "train; not in checked departures")
-            continue
+            #continue
 
         delay = int(connection["departure"]["delay"])
         if delay > 0:
-            events.append(create_event("delay", delay))
+            events.append(create_event(departure_time, "delay", delay))
 
         cancelled = int(connection["departure"]["canceled"])
         if cancelled > 1:
-            events.append(create_event("delay"))
+            events.append(create_event(departure_time, "delay"))
 
     return events
